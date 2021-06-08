58d85ff5105723438e13dcb7ebecf12f0e1369a5
David Saah <dasorange.hope@gmail.com>
2021-06-08 00:00:03 +0000

    Imported and rendering Home component in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import './Styles/Bootstrap/bootstrap.min.css'
+import Home from './Components/Home.js'
 
 function App() {
   return (
     <div>
-      
+      <Home />
     </div>
   );
 }
